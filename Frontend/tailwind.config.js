8fb9780910ad264f7fd69b6f33cf9522569c20b8
Dutga-p <david.camilo.ordonez@correounivalle.edu.co>
2023-11-28 16:02:13 -0500

    14 commit

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+],
+  theme: {
+    extend: {
+      colors:{
+        'orange-button':'#ff561e'
+      }
+    },
+    boxShadow: {
+      navbar: "0px 10px 8px 0px rgba(3, 3, 4, 0.03), 0 1px 2px -1px rgba(3, 3, 4, 0.03)",
+    }
+  },
+  plugins: [
+    function({addUtilities}){
+      const estendUnderline={
+        '.underline':{
+          'textDecoration':'underline',
+          'text-decoration-color': '#ff561e',
+          'transition-duration': '300 ease-in-out',
+          'borderBottom': '2px solid #ff561e',
+        },
+      }
+      addUtilities(estendUnderline)
+    }
+  ],
+}
